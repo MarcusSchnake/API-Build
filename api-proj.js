84811e55560bf74a6d865ea7101693be060ee5db
Marcus Schnake <marcusschnake@Marcuss-MBP.lan>
2021-09-16 18:11:27 -0400

    first commit api build

---
+++ api-proj.js
@@ -0,0 +1,16 @@
+const baseURL='https://pokeapi.co/api/v2/';
+let url;
+
+const pokemonName = document.querySelector('.name');
+const pokemonGeneration = document.querySelector('generation');
+const pokemonEffect = document.querySelector('effect');
+const submitBtn = document.querySelector('.submit');
+const nextBtn = document.querySelector('.next');
+const previousBtn = document.querySelector('.prev');
+const nav = document.querySelector('nav');
+const section = document.querySelector('section');
+
+
+
+
+url=`${url} + `
